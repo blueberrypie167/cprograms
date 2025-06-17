986db62e128d694dbcdd5790a316c5a3426b3b02
blueberrypie167 <82982605+blueberrypie167@users.noreply.github.com>
2025-06-17 05:33:27 +0300

    Update findclosestnumber.cpp
    updated, only takes one parameter using vector library

--- findclosestnumber.cpp
+++ findclosestnumber.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 using namespace std;
 // i've started practicing leetcode dsa questions
 // i will later figure out how to work the function without inputting its size
 // algorithm for finding closest number to 0
-int findclosestNumber(int set[], int n){
+int findclosestNumber(vector<int> set){
   int closest = set[0];
+  int n = set.size();
   for(int i = 0;i < n;i++){
-    if(abs(set[i]) < abs(closest)){
+    if(abs(set[i]) < abs(closest) || nums[i] == abs(closest){
       closest = set[i];  
     }
   }
@@ -15,8 +17,7 @@ int findclosestNumber(int set[], int n){
 } 
 
 int main(){
-  int set[5] = {-1,-2,3,4,-6};
-  int n = sizeof(set) / sizeof(set[0]);
-  cout << findclosestNumber(set, n);
+  vector <int> set = {-1,-2,3,4,-6};
+  cout << findclosestNumber(set);
 
 }
