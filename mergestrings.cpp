4f741c3b2b4c8ee17f54b35f9ae006f0b6991cc9
blueberrypie167 <82982605+blueberrypie167@users.noreply.github.com>
2025-06-18 21:13:10 +0300

    Create mergestrings.cpp

---
+++ mergestrings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string mergestrings(string word1, string word2){
+  
+}
+
+int main(){
+  string word1, word2;
+  cout << "1st word: ";
+  cin >> word1; 
+  cout << "2nd word: ";
+  cin >> word2;
+  mergestrings(word1, word2);
+  
+}
